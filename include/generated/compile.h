b4c7def39bfa4d080259017c6e4bc293fb5bc795
root <root@cross>
2015-01-06 00:42:47 +0100

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 SMP PREEMPT Mon Dec 15 15:45:55 CET 2014"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "cross"
+#define LINUX_COMPILER "gcc version 4.7.2 (Debian 4.7.2-5) "
